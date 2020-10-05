e59738a8ac33697f0ad745bcb75b1165843ba18e
gooanq <ivanguzikxv@gmail.com>
2020-10-05 20:14:50 +0300

    feat: implemented task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+'use strict'
+module.exports = check;
+
+function check(str, bracketsConfig) {
+  let openSymbol = [];
+  let closeSymbol = [];
+  bracketsConfig.forEach(element => {
+    openSymbol.push(element[0]);
+    closeSymbol.push(element[1]);
+  });
+
+  let symbolsCheck = [];
+
+  LOOP:
+  for (let i = 0; i < str.length; i++) {
+    if (openSymbol.indexOf(str[i]) !== -1 && closeSymbol.indexOf(str[i]) !== -1) {
+      for (let j = i - 1; j => 0; j--) {
+        if (j === -1) {
+          break;
+        }
+        else if (symbolsCheck[j].indexSymbol === closeSymbol.indexOf(str[i]) &&
+          symbolsCheck[j].closed === false) {
+
+          symbolsCheck[j].closed = true;
+          symbolsCheck.push({
+            indexSymbol: closeSymbol.indexOf(str[i]),
+            closed: true,
+          });
+
+          for (let k = j; k <= i; k++) {
+            if (symbolsCheck[k].closed === false) {
+              return false;
+            }
+          }
+          continue LOOP;
+        }
+      }
+      symbolsCheck.push({
+        indexSymbol: openSymbol.indexOf(str[i]),
+        closed: false
+      })
+    }
+    else if (openSymbol.indexOf(str[i]) !== -1) {
+      symbolsCheck.push({
+        indexSymbol: openSymbol.indexOf(str[i]),
+        closed: false
+      })
+    }
+    else if (closeSymbol.indexOf(str[i]) !== -1) {
+      for (let j = i - 1; j => 0; j--) {
+        if (j === -1) break;
+        else if (symbolsCheck[j].indexSymbol === closeSymbol.indexOf(str[i]) &&
+          symbolsCheck[j].closed === false
+        ) {
+          symbolsCheck[j].closed = true;
+          symbolsCheck.push({
+            indexSymbol: closeSymbol.indexOf(str[i]),
+            closed: true,
+          });
+
+          for (let k = j; k <= i; k++) {
+            if (symbolsCheck[k].closed === false) {
+              return false;
+            }
+          }
+          continue LOOP;
+        }
+      }
+
+      return false;
+    }
+  }
+
+  for (const element of symbolsCheck) {
+    if (element.closed === false) {
+      return false;
+    }
+  };
+
+  return true;
 }
